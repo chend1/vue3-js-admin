7f25421f994134b58a60875aacc497a28cd344a0
chend1 <1752837807@qq.com>
2023-08-10 10:15:22 +0800

    fix: 解决代理错误问题

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-export default defineConfig((mode) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname);
   return {
     plugins: [vue()],
